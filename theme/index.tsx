12f7c29bca31a3af064cf95717749e640eb06272
matin <kingman.mza@gmail.com>
2023-08-02 23:02:50 +0330

    1-add local font (iran yekan) to project

--- theme/index.tsx
+++ theme/index.tsx
@@ -2,7 +2,18 @@ import { createTheme } from "@mui/material";
 import { faIR } from "@mui/material/locale";
 import palette from "./palette";
 import components from "./components";
+import { yekan } from "@/app/font";
 
-const theme = createTheme({ direction: "rtl", palette, components }, faIR);
+const theme = createTheme(
+  {
+    direction: "rtl",
+    palette,
+    components,
+    typography: {
+      fontFamily: yekan.style.fontFamily,
+    },
+  },
+  faIR
+);
 
 export default theme;
